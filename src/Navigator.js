700b6162b1120e5edaf84593c45223f92ba1ff3d
HandsomeRookie <35836911+a-happy-sigledog@users.noreply.github.com>
2024-09-09 10:08:21 +0800

    Update Navigator.js
    update

--- src/Navigator.js
+++ src/Navigator.js
@@ -35,9 +35,8 @@ import {
   StyleSheet,
   TVEventHandler,
   View,
-  ViewPropTypes,
 } from 'react-native';
-
+import { ViewPropTypes } from 'deprecated-react-native-prop-types';
 var AnimationsDebugModule = NativeModules.AnimationsDebugModule;
 var InteractionMixin = require('./InteractionMixin');
 var NavigationContext = require('./NavigationContext');
